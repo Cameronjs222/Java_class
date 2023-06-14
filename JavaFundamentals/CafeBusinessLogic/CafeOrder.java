6ccb60b05026bdb12efb01514d4ca596527aec76
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-06-14 11:13:28 -0700

    added controller

--- JavaFundamentals/CafeBusinessLogic/CafeOrder.java
+++ JavaFundamentals/CafeBusinessLogic/CafeOrder.java
@@ -6,4 +6,28 @@ public class CafeOrder {
     public double total;
     public boolean ready;
     public ArrayList<String> items = new ArrayList<String>();
+
+    public CafeOrder(String name, double total, boolean ready, ArrayList<String> items) {
+        this.name = name;
+        this.total = total;
+        this.ready = ready;
+        this.items = items;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+    }
+    
 }

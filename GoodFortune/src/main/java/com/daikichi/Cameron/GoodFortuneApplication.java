d3a70d1f2498835a43b75790f4e5c585c74c5da4
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-06-20 16:00:35 -0700

    added TravelController and Lotto Controller

--- GoodFortune/src/main/java/com/daikichi/Cameron/GoodFortuneApplication.java
+++ GoodFortune/src/main/java/com/daikichi/Cameron/GoodFortuneApplication.java
@@ -1,7 +1,11 @@
+//this is the complete version
+
+
 package com.daikichi.Cameron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,3 +37,29 @@ class HomeController {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
     }
 }
+@RestController
+@RequestMapping("/travel")
+class TravelController {
+    @RequestMapping("/{location}")
+    public String location(@PathVariable("location") String location) {
+
+    	return "Congratulations, you will soon be traveling to " + location;
+
+    }
+}
+
+@RestController
+@RequestMapping("/lotto")
+class LottoController {
+    @RequestMapping("/{num}")
+    public String lottoNumber(@PathVariable("num") String number) {
+        int num = Integer.parseInt(number);
+
+        if (num % 2 == 0) {
+            return "You will take a grand journey in the near future, but be weary of tempting offers";
+        } else {
+            return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+        }
+    }
+}
+

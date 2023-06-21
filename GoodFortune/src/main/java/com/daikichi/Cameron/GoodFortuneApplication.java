03504f552139f7ca1e0f3104e49092fac4ed1f28
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-06-20 18:08:53 -0700

    Got rid of int conversion in lotto route

--- GoodFortune/src/main/java/com/daikichi/Cameron/GoodFortuneApplication.java
+++ GoodFortune/src/main/java/com/daikichi/Cameron/GoodFortuneApplication.java
@@ -37,7 +37,6 @@ class HomeController {
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
     }
 }
-@RestController
 @RequestMapping("/travel")
 class TravelController {
     @RequestMapping("/{location}")
@@ -48,14 +47,12 @@ class TravelController {
     }
 }
 
-@RestController
 @RequestMapping("/lotto")
 class LottoController {
     @RequestMapping("/{num}")
-    public String lottoNumber(@PathVariable("num") String number) {
-        int num = Integer.parseInt(number);
-
-        if (num % 2 == 0) {
+    public String lottoNumber(@PathVariable("num") double number) {
+    	
+        if (number % 2 == 0) {
             return "You will take a grand journey in the near future, but be weary of tempting offers";
         } else {
             return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";

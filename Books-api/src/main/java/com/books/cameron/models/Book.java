2b19aacb6a7026a16783992e5ea114b00f05c89c
Cameron Shaffer <118939035+Cameronjs222@users.noreply.github.com>
2023-06-28 13:43:02 -0700

    test

--- Books-api/src/main/java/com/books/cameron/models/Book.java
+++ Books-api/src/main/java/com/books/cameron/models/Book.java
@@ -2,6 +2,7 @@ package com.books.cameron.models;
 
 import jakarta.persistence.Entity;
 
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
